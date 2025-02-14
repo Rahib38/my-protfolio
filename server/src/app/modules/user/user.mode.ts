cc1c81ff3c89093977556008d379d3aec319560c
Nadimul Rahib <nadimulrahib38@gmail.com>
2025-02-13 19:23:32 -0800

    added

---
+++ server/src/app/modules/user/user.mode.ts
@@ -0,0 +1,50 @@
+import bcrypt from 'bcrypt';
+import { model, Schema } from 'mongoose';
+import config from '../../config';
+import { TUser } from './user.interface';
+const userSchema = new Schema<TUser>(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      unique: true,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      trim: true,
+      select: false,
+    },
+    role: {
+      type: String,
+      enum: ['user', 'admin'],
+      default: 'user',
+    },
+    isBlocked: {
+      type: Boolean,
+      default: false,
+      required: true,
+    },
+  },
+  { timestamps: true },
+);
+userSchema.pre('save', async function (next) {
+  // eslint-disable-next-line @typescript-eslint/no-this-alias
+  const user = this; // doc
+  // hashing password and save into DB
+
+  user.password = await bcrypt.hash(
+    user.password,
+    Number(config.bcrypt_salt_rounds),
+  );
+
+  next();
+});
+const User = model<TUser>('User', userSchema);
+
+export default User;
