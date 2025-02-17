a9bf120331dfd1f45b2c7ce4d683b927172d84ef
Nadimul Rahib <nadimulrahib38@gmail.com>
2025-02-17 00:37:45 -0800

    added

--- client/src/app/dashboard/blogs/page.tsx
+++ client/src/app/dashboard/blogs/page.tsx
@@ -13,8 +13,8 @@ const BlogPage = async () => {
       <div className="flex justify-end">
         <AddBlogModal></AddBlogModal>
       </div>
-      <h2>All Blogs</h2>
-      <div className="grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-10" >
+      <h2 className="text-3xl font-semibold text-center ">All Blogs</h2>
+      <div className="grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-10 mt-5 lg:px-5" >
        
         {blogs?.data?.map((blog:any) => (
           <BlogCard key={blog.id} blog={blog}></BlogCard>
--- server/src/app.ts
+++ server/src/app.ts
@@ -1,21 +1,22 @@
-import cors from "cors";
-import express, { Application, Request, Response } from "express";
-import { StatusCodes } from "http-status-codes";
-import { globalErrorHandler } from "./app/middlewares/globalErrorHandlers";
-import blogRouter from "./app/modules/blog/blog.route";
-import projectsRouter from "./app/modules/projects/project.routes";
+import cors from 'cors';
+import express, { Application, Request, Response } from 'express';
+import { StatusCodes } from 'http-status-codes';
+import { globalErrorHandler } from './app/middlewares/globalErrorHandlers';
+import blogRouter from './app/modules/blog/blog.route';
+import messageRouter from './app/modules/message/message.route';
+import projectsRouter from './app/modules/projects/project.routes';
 const app: Application = express();
 // const port = 3000;
 app.use(express.json());
 app.use(cors());
 
-
-app.use('/api/blogs',blogRouter)
-app.use('/api/projects',projectsRouter)
-app.get("/", (req: Request, res: Response) => {
-  res.send("Hello World!");
+app.use('/api/blogs', blogRouter);
+app.use('/api/projects', projectsRouter);
+app.use('/api/message', messageRouter);
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello World!');
 });
-app.use(globalErrorHandler)
+app.use(globalErrorHandler);
 app.use('*', (req: Request, res: Response) => {
   res.status(404).json({
     status: false,
