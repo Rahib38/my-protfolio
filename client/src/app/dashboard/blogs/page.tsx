1a3a0ea721ff524c4de8d73b09dd5c019bfe4975
Nadimul Rahib <nadimulrahib38@gmail.com>
2025-02-16 18:47:41 -0800

    added

--- client/src/app/dashboard/blogs/page.tsx
+++ client/src/app/dashboard/blogs/page.tsx
@@ -1,12 +1,27 @@
-import AddBlog from '@/app/components/blogs/AddBlog'
-import React from 'react'
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import AddBlogModal from "@/app/components/blogs/AddBlogModal";
+import BlogCard from "@/app/components/blogs/BlogCard";
 
-const BlogPage = () => {
+const BlogPage = async () => {
+  const res = await fetch("http://localhost:5001/api/blogs", {
+    cache: "no-store",
+  });
+  const blogs = await res.json();
+  console.log(blogs)
   return (
     <div>
-    <AddBlog></AddBlog>
+      <div className="flex justify-end">
+        <AddBlogModal></AddBlogModal>
+      </div>
+      <h2>All Blogs</h2>
+      <div className="grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-10" >
+       
+        {blogs?.data?.map((blog:any) => (
+          <BlogCard key={blog.id} blog={blog}></BlogCard>
+        ))}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default BlogPage
+export default BlogPage;
